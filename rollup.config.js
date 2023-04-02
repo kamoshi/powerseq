2905dbf8a8cf4ff5c5e036fe072e2104aabaac47
Maciej Jur <maciej@kamoshi.org>
2023-04-03 01:24:30 +0200

    Include TS compilation in Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+
 /** @type {import("rollup").RollupOptions} */
 const options = {
-	input: 'dist/esm_es5/src/index.js',
+	input: 'src/index.ts',
 	output: {
     name: 'powerseq',
 		file: 'npmpackage/bundles/powerseq.es5.js',
     sourcemap: true,
 		format: 'umd',
 	},
+	plugins: [
+		typescript({tsconfig: 'tsconfig.esm_es5.json'}),
+		terser(),
+	]
 };
 
 export default options;
